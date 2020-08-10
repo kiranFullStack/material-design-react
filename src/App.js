6a9d50bee67dc9964a009b2a389a5de9b380ed38
kiranFullStack <kiran.fullstack@outlook.com>
2020-08-10 18:03:22 +0530

    installed material design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react"
+import Button from "@material-ui/core/Button"
 
 function App() {
   return (
     <div className="App">
       <h1>⭐️ Material Design ⭐️</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   )
 }
